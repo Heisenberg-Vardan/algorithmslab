946ab87015652fa8834ad7b51430c67a9c9ec8e2
Heisenberg-Vardan <vardannarula@gmail.com>
2020-10-03 01:51:16 +0530

    Added BFS Using STL

--- bfs.cpp
+++ bfs.cpp
@@ -1,113 +1,65 @@
-#include<iostream>
-using namespace std;
 
-void get_matrix(int n, int W[20][20])
-{
-  cout << "Enter adjacency matrix ";
-  for(int i=1;i<=n;i++)
-    for(int j=1;j<=n;j++)
-      cin >> W[i][j];
-}
-
-void BFS(int n, int W[20][20])
-{
-    int source;
-    int visited[20];
-    int queue[20];
-    int front = 0, rear = -1, u ,v;
-
-    for(int i=1;i<=n;i++)
-        visited[i]=0;
-
-    cout << " Enter the source node: ";
-    cin >> source;
-    visited[source]=1;
-
-    queue[++rear] = source;
-    while(front <= rear)
-    {
-      u = queue[front++];
-
-      for(v=1; v<=n; v++)
-      {
-        if(W[u][v] && !visited[v])
-        {
-          queue[++rear] = v;
-          visited[v] = 1;
-        }
-      }
-    }
-
-    for(int i=1; i<=n; i++)
-    {
-      if(visited[i])
-        cout << "Node " << i <<" is reachable. " << endl;
-      else
-        cout << "Node " << i <<" is not reachable. " <<endl;
-    }
-
-}
-
-int main()
-{
-  int n,W[20][20];
-
-  cout << " Enter number of vertices: ";
-  cin >> n;
-
-  get_matrix(n, W);
-  BFS(n,W);
+// A Quick implementation of BFS using 
+// vectors and queue 
+#include <bits/stdc++.h> 
+#define pb push_back 
   
+using namespace std; 
+  
+vector<bool> v; 
+vector<vector<int> > g; 
+  
+void edge(int a, int b) 
+{ 
+    g[a].pb(b); 
+  
+    // for undirected graph add this line 
+    // g[b].pb(a); 
+} 
+  
+void bfs(int u) 
+{ 
+    queue<int> q; 
+  
+    q.push(u); 
+    v[u] = true; 
+  
+    while (!q.empty()) { 
+  
+        int f = q.front(); 
+        q.pop(); 
+  
+        cout << f << " "; 
+  
+        // Enqueue all adjacent of f and mark them visited  
+        for (auto i = g[f].begin(); i != g[f].end(); i++) { 
+            if (!v[*i]) { 
+                q.push(*i); 
+                v[*i] = true; 
+            } 
+        } 
+    } 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    int n, e; 
+    cin >> n >> e; 
+  
+    v.assign(n, false); 
+    g.assign(n, vector<int>()); 
+  
+    int a, b; 
+    for (int i = 0; i < e; i++) { 
+        cin >> a >> b; 
+        edge(a, b); 
+    } 
+  
+    for (int i = 0; i < n; i++) { 
+        if (!v[i]) 
+            bfs(i); 
+    } 
+  
+    return 0; 
 }
-
-
-/* OUTPUT 1 
-
-Ashwanis-MacBook-Air:~ Ashwani$ ./a.out
- Enter number of vertices: 5
-Enter adjacency matrix          
-0 1 1 0 0 
-1 0 1 0 0
-1 1 0 0 0 
-0 0 0 0 1
-0 0 0 1 0
- Enter the source node: 1
-Node 1 is reachable. 
-Node 2 is reachable. 
-Node 3 is reachable. 
-Node 4 is not reachable. 
-Node 5 is not reachable.
-
-
-*/
-
-
-/* OUTPUT 2
-
-Ashwanis-MacBook-Air:~ Ashwani$ ./a.out
- Enter number of vertices: 10
-Enter adjacency matrix 
-0 0 1 1 1 0 0 0 0 0 
-0 0 0 0 1 1 0 0 0 0
-1 0 0 1 0 1 0 0 0 0
-1 0 1 0 0 0 0 0 0 0
-1 1 0 0 0 1 0 0 0 0
-0 1 1 0 1 0 0 0 0 0
-0 0 0 0 0 0 0 1 0 1
-0 0 0 0 0 0 1 0 1 0 
-0 0 0 0 0 0 0 1 0 1
-0 0 0 0 0 0 1 0 1 0
- Enter the source node: 1
-Node 1 is reachable. 
-Node 2 is reachable. 
-Node 3 is reachable. 
-Node 4 is reachable. 
-Node 5 is reachable. 
-Node 6 is reachable. 
-Node 7 is not reachable. 
-Node 8 is not reachable. 
-Node 9 is not reachable. 
-Node 10 is not reachable.
-
-
-*/
